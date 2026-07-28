beece7cc1e693ef5a2c0d3bebfdb2dc068e89831
yumin-pub <cbram.614@gmail.com>
2026-07-28 14:36:39 +0900

    refactor(button): clean up component and set safe button type

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,40 +1,33 @@
-// import type { ButtonHTMLAttributes, ReactNode } from 'react';
 import type { ButtonProps } from './Button.types';
 
-// type ButtonVariant = 'solid' | 'outline' | 'icon' | 'box';
-// type ButtonColor = 'primary' | 'secondary' | 'neutral';
-// type ButtonSize = 'lg' | 'md';
-
-// interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-//     children: ReactNode;
-//     variant?: ButtonVariant;
-//     color?: ButtonColor;
-//     size?: ButtonSize;
-//     }
-
 function Button({
-    children,
-    variant = 'solid',
-    color = 'primary',
-    size = 'md',
-    className = '',
-    ...props
-    }: ButtonProps) {
-    const buttonClassName = [
-        'ui-btn',
-        `ui-btn--${variant}`,
-        `ui-btn--${color}`,
-        `ui-btn--${size}`,
-        className,
-    ]
-        .filter(Boolean) // 빈값 제거 = 쓸모 없는 값 청소
-        .join(' ');
+	children,
+	variant = 'solid',
+	color = 'primary',
+	size = 'md',
+	type = 'button',
+	className = '',
+	...props
+}: ButtonProps) {
+	const buttonClassName = [
+		'ui-btn',
+		`ui-btn--${variant}`,
+		`ui-btn--${color}`,
+		`ui-btn--${size}`,
+		className,
+	]
+    .filter(Boolean)
+    .join(' ');
 
-    return (
-        <button className={buttonClassName} {...props}>
-        {children}
-        </button>
-    );
+	return (
+		<button
+		type={type}
+		className={buttonClassName}
+		{...props}
+		>
+		{children}
+		</button>
+	);
 }
 
 export default Button;
